12f3faa7d60e912a98fedc7d7846575952a6f992
Chidinma Nwodo <chidinmanwodo@usc-guestwireless-upc-newsc19134.usc.edu>
2019-07-29 10:30:29 -0700

    Added demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+# -----------------------------------------------------------------
+#              Name: Chidinma Nwodo
+#       Filename: demo.py
+#                 Date: 7/29/19
+# Description: sample python file
+#-----------------------------------------------------------------
+
+def main():
+
+    name = "Jordan"
+    print("Hello, my name is", name)
+
+if __name__ == "__main__":
+    main()
